17ae6b6183767b51b97258e2c406d11b14fe3fe3
SilverSoul <44640904+silvery107@users.noreply.github.com>
2021-12-21 14:13:51 +0800

    update

--- src/test_plan.cpp
+++ src/test_plan.cpp
@@ -4,13 +4,13 @@
 
 int main()
 {
-    double A_M = 10;
-    double V_M = 5;
+    double A_M = 5;
+    double V_M = 2;
     double xyz_pos[3] = { 1, 0, 0 };
 
     TPlanner test = TPlanner(A_M, V_M, xyz_pos);
     long totaltime = test.getPlanTime();
-    double s = test.getXCurve(3500);
+    double s = test.getXCurve(totaltime);
 
     std::cout << totaltime << "\n"
               << s << std::endl;
@@ -18,7 +18,7 @@ int main()
     TCurve test_old = TCurve(A_M, V_M);
     test_old.getCurveParam();
     totaltime = test_old.getTc()*1000;
-    s = test_old.getTCurve(900);
+    s = test_old.getTCurve(totaltime);
 
 
     std::cout << totaltime << "\n"
