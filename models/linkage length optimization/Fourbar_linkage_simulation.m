90020a6827b788f569735ddc7fe2dd9549fa95e7
Seas00n <60185289+Seas00n@users.noreply.github.com>
2022-01-05 20:58:23 +0800

    add

--- models/linkage length optimization/Fourbar_linkage_simulation.m
+++ models/linkage length optimization/Fourbar_linkage_simulation.m
@@ -21,30 +21,7 @@ for i=1:size(theta3,2)
     rend(:,i)=1/2*r1(:,i)+Lend*[cos(theta_end);-sin(theta_end)];
 end
 %Visualization
-for i=1:size(r1,2)
-    scatter(rend(1,i),rend(2,i),'LineWidth',5)
-    hold on
-    xlim([-1,1]);
-    ylim([-2,0]);
-    line([0,r1(1,i)],[0,r1(2,i)],'Linewidth',2);
-    line([r1(1,i),r2(1,i)],[r1(2,i),r2(2,i)],'Linewidth',2);
-    line([r2(1,i),x],[r2(2,i),0],'Linewidth',2);
-    line([r1(1,i)/2,rend(1,i)],[r1(2,i)/2,rend(2,i)],'Linewidth',2)
-    rectangle('Position',[0.058 -2 2 0.1+i*0.015],'FaceColor',[0 .5 .5],'EdgeColor','b',...
-    'LineWidth',3,'Curvature',0.4)
-    plot(rend(1,:),rend(2,:),'LineWidth',5)
-    hold off
-    drawnow;
-    F=getframe(gcf);
-    I=frame2im(F);
-    [I,map]=rgb2ind(I,256);
-    if i == 1
-        imwrite(I,map,'test.gif','gif','Loopcount',inf,'DelayTime',0.2);
-    else
-        imwrite(I,map,'test.gif','gif','WriteMode','append','DelayTime',0.2);
-    end
-    pause(0.2);
-end
+Visualization;
 %Function
 function [theta1,theta2,theta3]=solve_triangle(x1,x2,x3)
  theta1=acos((x2^2+x3^2-x1^2)/(2*x2*x3));
